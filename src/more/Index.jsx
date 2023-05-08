1f91af26933f72ad1a2430ef7d5278079b80bed7
devryyeong <thisisme20@daum.net>
2023-05-08 15:34:37 +0900

    Chore: 파일 이름 수정

--- src/more/Index.jsx
+++ src/more/Index.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useCallback  } from "react";
+import { useState, useMemo, useCallback } from "react";
 import { Div, Input, Button, P, Span } from "../styles";
 import Child from "./Child";
 
@@ -53,6 +53,6 @@ const Index = () => {
       </Div>
     </>
   );
-}
+};
 
 export default Index;
